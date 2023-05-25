c8899788fde9ffd1979c8b09723ab1f97e6b5ce5
hussein.jamoul <hussein.jamoul@turbinekreuzberg.com>
2023-05-25 22:35:27 +0200

    [UPDATE] Input box capturing and fetch two paragraph from search

--- search_engine_executor.py
+++ search_engine_executor.py
@@ -8,6 +8,8 @@ class SearchEngineExecutor:
         self.__search_results = {}
 
     def document_search(self, search_index):
+        print("===================")
+        print(search_index)
         self.__wiki_search_url = f'https://en.wikipedia.org/wiki/{search_index}'
         request_result = requests.get(self.__wiki_search_url)
 
@@ -17,14 +19,15 @@ class SearchEngineExecutor:
         doc_title = self.__get_document_title(soup)
         self.__search_results["title"] = doc_title
 
-        doc_paragraph = self.__get_first_paragraph(soup)
-        self.__search_results["paragraph"] = doc_paragraph
+        doc_paragraph = self.__get_two_paragraph(soup)
+        self.__search_results["paragraph1"] = doc_paragraph[0]
+        self.__search_results["paragraph2"] = doc_paragraph[1]
 
         return self.__search_results
 
     def __get_document_title(self, soup):
         return soup.find('h1').text
 
-    def __get_first_paragraph(self, soup):
-        return soup.find('p').text
+    def __get_two_paragraph(self, soup):
+        return soup.findAll('p')[0].text, soup.findAll('p')[1].text
 
--- search_gui.py
+++ search_gui.py
@@ -9,16 +9,18 @@ class GUI:
         self.__search_engine_executor = search_engine_executor
         self.__unified_background = "#333333"
         self.__unified_purple_color = "#8A2BE2"
+        self.__search_input = None
+        self.__output_label = None
+        self.__compile_window_sections()
 
-        self.___compile_window_sections()
-
-    def ___compile_window_sections(self):
+    def __compile_window_sections(self):
         self.__window.title("GUI created with tkinter")
         self.__window.geometry("800x800")
         self.__window.configure(bg=self.__unified_background)
 
         self.__build_header_section()
         self.__build_search_section()
+        self.__build_output_section()
 
     def __build_header_section(self):
         bold25 = font.Font(size=25, weight=font.BOLD)
@@ -31,29 +33,57 @@ class GUI:
     def __build_search_section(self):
         search_frame = Frame(self.__window, bg=self.__unified_background)
 
-        search_input = Entry(search_frame, font=("arial", 20), highlightthickness=4, width=40, bg="#706f6f", fg="white")
-        search_input.config(highlightbackground="grey", highlightcolor="grey")
-        search_input.grid(row=0, column=0)
+        self.__search_input = Entry(search_frame, font=("arial", 20), highlightthickness=4, width=40, bg="#706f6f",
+                                    fg="white")
+        self.__search_input.config(highlightbackground="grey", highlightcolor="grey")
+        self.__search_input.grid(row=0, column=0)
 
         buttonText = StringVar()
-        search_button = Button(search_frame, textvariable=buttonText, font=font.Font(size=17, weight=font.BOLD),
-                               bg=self.__unified_purple_color, fg="white")
+        search_button = Button(search_frame,
+                               textvariable=buttonText,
+                               font=font.Font(size=17, weight=font.BOLD),
+                               bg=self.__unified_purple_color, fg="white",
+                               command=self.search
+                               )
+
         buttonText.set("Search")
         search_button.grid(row=0, column=1, padx=10)
 
         search_frame.pack(pady=40)
 
-        search_index = search_input.get()
-        print(search_index)
+    def search(self):
+        search_index = self.__search_input.get()
+        search_results = self.__search_engine_executor.document_search(search_index)
+        title = self.filter_search_result_title(search_results)
+        paragraph1 = self.filter_search_result_paragraph1(search_results)
+        paragraph2 = self.filter_search_result_paragraph2(search_results)
+        self.__output_label.config(text=f"{title}\n{paragraph1}\n{paragraph2}")
 
-    def search(self, search_index):
-        self.__search_engine_executor.document_search(search_index)
+        print(title)
+        print(paragraph)
+
+    def __build_output_section(self):
+        self.__output_label = Label(
+            self.__window,
+            text="Hey There! Search something :)",
+            background="white",
+            font=("arial", 20),
+            highlightthickness=4,
+            width=50,
+            height=10,
+            wraplength=800
+        )
+        self.__output_label.config(highlightbackground="grey", highlightcolor="grey")
+        self.__output_label.pack()
 
     def filter_search_result_title(self, search_results):
         return search_results.get("title")
 
-    def filter_search_result_paragraph(self, search_results):
-        return search_results.get("paragraph")
+    def filter_search_result_paragraph1(self, search_results):
+        return search_results.get("paragraph1")
+
+    def filter_search_result_paragraph2(self, search_results):
+        return search_results.get("paragraph2")
 
 
 def main():
